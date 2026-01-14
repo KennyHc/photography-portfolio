85c750330a359f478b0d840e4db2fa96310fedf3
Kenny He <kennyjiaxi@hotmail.com>
2026-01-14 22:00:33 +0100

    add photos

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,18 +1,25 @@
 // 1. Import the images at the top
-import food1 from '../assets/coffee_food.jpg'; // Adjust filenames to match yours
+import food1 from '../assets/coffee.jpg'; // Adjust filenames to match yours
 import scene1 from '../assets/fireworks.jpg';
 import event1 from '../assets/fireworks_wedding.jpg';
 import urban1 from '../assets/gz_night.jpg';
-import food2 from '../assets/matcha_cookie.jpg';
+import food2 from '../assets/matcha.jpg';
 import urban2 from '../assets/umbrella.jpg';
 import arc1 from '../assets/basilica.jpg';
 import nat1 from '../assets/trees.jpg';
+import food3 from '../assets/shrimp.jpg';
+import urban3 from '../assets/construction.jpg';
+import event2 from '../assets/bride.jpg';
+import portrait1 from '../assets/street_portrait.jpg';
+import event3 from '../assets/parade.jpg';
+import food4 from '../assets/tea.jpg';
+import food5 from '../assets/latte.jpg';
 
 export const photos = [
   { 
     id: 1, 
     category: 'Food', 
-    src: food1, // <--- Use the imported variable
+    src: food1,
     title: 'Coffee' 
   },
   { 
@@ -57,6 +64,51 @@ export const photos = [
     src: nat1,
     title: 'Behind the Trees' 
   },
+  { 
+    id: 9, 
+    category: 'Food', 
+    src: food3,
+    title: '咸蛋黄炒虾' 
+  },
+  { 
+    id: 10, 
+    category: 'Urban', 
+    src: urban3,
+    title: 'High-rise contruction' 
+  },
+  { 
+    id: 11, 
+    category: 'Event', 
+    src: event2,
+    title: 'Bride' 
+  },
+  { 
+    id: 12, 
+    category: 'Portraits', 
+    src: portrait1,
+    title: 'Street Portrait' 
+  },
+  { 
+    id: 13, 
+    category: 'Event', 
+    src: event3,
+    title: 'Reyes Parade' 
+  },
+  { 
+
+    id: 14, 
+    category: 'Food', 
+    src: food4,
+    title: 'Tea Serving' 
+  },
+  { 
+    
+    id: 15, 
+    category: 'Food', 
+    src: food5,
+    title: 'Latte' 
+  },
+
 ];
 
 
